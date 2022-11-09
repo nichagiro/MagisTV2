ff61026c45b9e7994c7cfe89a305d58f341289fc
Nicolas Chamorro <nichagiro@gmail.com>
2022-11-09 11:25:25 -0500

    FIXED RULES WARNING NEXT

--- components/publicitarios/NavidadPromo.jsx
+++ components/publicitarios/NavidadPromo.jsx
@@ -22,19 +22,42 @@ const NavidadPromo = () => {
                 data-target="#exampleModal">
                 click popup navidad
             </button>
-            <div style={{ zIndex: 10000 }} className="modal fade" id="exampleModal" tabIndex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+            <div
+                style={{ zIndex: 10000 }}
+                className="modal fade"
+                id="exampleModal"
+                tabIndex="-1"
+                role="dialog"
+                aria-labelledby="exampleModalLabel"
+                aria-hidden="true"
+            >
                 <div className="modal-dialog" role="document">
                     <div className="modal-content">
                         <div className="modal-header">
                             <h5 className="modal-title">Magist Tv International</h5>
-                            <button type="button" className="close" data-dismiss="modal" aria-label="Close">
+                            <button
+                                type="button"
+                                className="close"
+                                data-dismiss="modal"
+                                aria-label="Close">
                                 <i className="fas fa-times-circle"></i>
                             </button>
                         </div>
                         <div>
-                            <img src='img/navidadPromo.webp' className="img-fluid" alt="Navidad magis tv international" />
+                            <picture>
+                                <source srcSet="/img/navidadPromo.webp" type="image/webp" />
+                                <img
+                                    src='/img/navidadPromo.webp'
+                                    className="img-fluid"
+                                    alt="Navidad magis tv international"
+                                    loading="lazy"
+                                />
+                            </picture>
                         </div>
-                        <div className="d-flex justify-content-end px-2" style={{ background: '#174960' }}>
+                        <div
+                            className="d-flex justify-content-end px-2"
+                            style={{ background: '#174960' }}
+                        >
                             <WhatsAppButtom
                                 label='Lo quiero!'
                                 phone={584126679598}
