a791329b246cd87b5651fd4d55260ff2c0781aa9
Nicolas Chamorro <nichagiro@gmail.com>
2023-08-11 16:36:14 -0500

    update apps

--- containers/home/Descargar.jsx
+++ containers/home/Descargar.jsx
@@ -1,21 +1,21 @@
 import React from 'react'
 
-const Descargar = ({className}) => {
-    return (
-        <section>
-            <button
-                data-toggle="modal"
-                data-target="#app_magis"
-                id="btn-download"
-                className={className}
-            >
-                Descargar
-            </button>
-        </section>)
+const Descargar = ({ className }) => {
+  return (
+    <section>
+      <button
+        data-toggle="modal"
+        data-target="#app_magis"
+        id="btn-download"
+        className={className}
+      >
+        Descargar
+      </button>
+    </section>)
 }
 
 export default Descargar
 
 Descargar.defaultProps = {
-    className : 'mt-2 dg-red px-5 py-3'
+  className: 'mt-2 dg-red px-5 py-3'
 }
